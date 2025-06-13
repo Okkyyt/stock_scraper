7a6708fd6c3006e70e774e94568c9d30f86dd3bb
Okkyyt <okky.161227@gmail.com>
2025-06-13 22:37:21 +0900

    fix: cli

--- src/stock_scraper/domain/cli.py
+++ src/stock_scraper/domain/cli.py
@@ -7,14 +7,15 @@ from stock_scraper.domain.time_period import parse_time_period
 
 @dataclass(slots=True)
 class CLIConfig:
-    command: str
     symbol: str
     bins: str
     interval: Optional[str] = None
     range_: Optional[str] = None
 
 
-def add_parser_arguments(parser):
+def build_parser():
+    parser = argparse.ArgumentParser()
+
     # 銘柄
     parser.add_argument(
         "-s",
@@ -29,34 +30,15 @@ def add_parser_arguments(parser):
         type=str,
         required=True,
     )
-    return parser
-
-
-def build_parser():
-    parser = argparse.ArgumentParser()
-
-    # サブパーサーの追加
-    sub_parsers = parser.add_subparsers(dest="command", required=True)
-
-    # サブコマンドの定義
-    quote = sub_parsers.add_parser(
-        "quote",
-    )
-    indicators = sub_parsers.add_parser(
-        "indicators",
-    )
-
-    # 引数の追加
-    quote = add_parser_arguments(quote)
-    indicators = add_parser_arguments(indicators)
-
-    indicators.add_argument(
+    # ローソク株価の足間隔
+    parser.add_argument(
         "-i",
         "--interval",
         type=str,
         default=None,
     )
-    indicators.add_argument(
+    # データの取得範囲
+    parser.add_argument(
         "-r",
         "--range",
         dest="range_",  # rangeは予約語
